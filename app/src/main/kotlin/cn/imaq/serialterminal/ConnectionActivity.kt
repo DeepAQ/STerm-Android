82d074eb934fa5a40eefb4f08895e0c8a8c43da3
DeepAQ <i@aq741.com>
2017-04-15 16:10:21 +0800

    Add: logic for open USB serial port

--- app/src/main/kotlin/cn/imaq/serialterminal/ConnectionActivity.kt
+++ app/src/main/kotlin/cn/imaq/serialterminal/ConnectionActivity.kt
@@ -1,7 +1,12 @@
 package cn.imaq.serialterminal
 
+import android.content.Context
+import android.hardware.usb.UsbManager
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import cn.wch.ch34xuartdriver.CH34xUARTDriver
 import kotlinx.android.synthetic.main.activity_connection.*
 
 class ConnectionActivity : AppCompatActivity() {
@@ -11,6 +16,21 @@ class ConnectionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_connection)
         spinnerBaudrate.setSelection(5) // 9600
         spinnerDataBits.setSelection(3) // 8
+
+        Driver.driver = CH34xUARTDriver(
+                getSystemService(Context.USB_SERVICE) as UsbManager,
+                this, "${BuildConfig.APPLICATION_ID}.USB_PERMISSION"
+        )
+    }
+
+    fun onOpenButtonClicked(v: View) {
+        with (Driver.driver!!) {
+            if (UsbFeatureSupported() && ResumeUsbList() == 0 && UartInit()) {
+                Snackbar.make(v, "Open port succeeded!", Snackbar.LENGTH_LONG).show()
+                return
+            }
+        }
+        Snackbar.make(v, "Open port failed!", Snackbar.LENGTH_LONG).show()
     }
 
 }
---
+++ app/src/main/kotlin/cn/imaq/serialterminal/Driver.kt
@@ -0,0 +1,12 @@
+package cn.imaq.serialterminal
+
+import cn.wch.ch34xuartdriver.CH34xUARTDriver
+
+/**
+ * Created by adn55 on 2017/4/15.
+ */
+object Driver {
+
+    var driver: CH34xUARTDriver? = null
+
+}
