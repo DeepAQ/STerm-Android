62456e285d56a2420b88d4c75638f5ef0c6ff662
DeepAQ <i@aq741.com>
2017-04-16 02:48:38 +0800

    Add: A stub socket server to emulate the serial port

--- app/src/main/kotlin/cn/imaq/serialterminal/TerminalActivity.kt
+++ app/src/main/kotlin/cn/imaq/serialterminal/TerminalActivity.kt
@@ -3,6 +3,8 @@ package cn.imaq.serialterminal
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_terminal.*
+import kotlinx.android.synthetic.main.content_terminal.*
+import java.net.ServerSocket
 
 class TerminalActivity : AppCompatActivity() {
 
@@ -10,6 +12,32 @@ class TerminalActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_terminal)
         setSupportActionBar(toolbar)
+        editTerm.keyListener = null
+
+        Thread {
+            val ss = ServerSocket(12345)
+            while (true) {
+                val cs = ss.accept()
+                Thread {
+                    val ins = cs.getInputStream()
+                    while (true) {
+                        val buf = ByteArray(1024)
+                        val len = ins.read(buf)
+                        if (len < 0) {
+                            break
+                        } else {
+                            runOnUiThread {
+                                val s = String(buf, 0, len)
+                                editTerm.append(s)
+                                //editTerm.scrollTo(0, editTerm.bottom)
+                            }
+                        }
+                    }
+                    ins.close()
+                    cs.close()
+                }.start()
+            }
+        }.start()
     }
 
 }
